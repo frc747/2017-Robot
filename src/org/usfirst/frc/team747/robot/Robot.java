d132b06511380f8105f300fd598e14ff992c9243
Tyler Roussos <troussos@ITS.JNJ.com>
2017-01-10 10:41:52 -0500

    Added Driver station enum maps

--- src/org/usfirst/frc/team747/robot/Robot.java
+++ src/org/usfirst/frc/team747/robot/Robot.java
@@ -1,15 +1,15 @@
 
 package org.usfirst.frc.team747.robot;
 
+import org.usfirst.frc.team747.robot.commands.ExampleCommand;
+import org.usfirst.frc.team747.robot.subsystems.ExampleSubsystem;
+
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
-import org.usfirst.frc.team747.robot.commands.ExampleCommand;
-import org.usfirst.frc.team747.robot.subsystems.ExampleSubsystem;
 
 /**
  * The VM is configured to automatically run this class, and to call the
---
+++ src/org/usfirst/frc/team747/robot/maps/Robot.java
@@ -0,0 +1,5 @@
+package org.usfirst.frc.team747.robot.maps;
+
+public enum Robot {
+
+}
