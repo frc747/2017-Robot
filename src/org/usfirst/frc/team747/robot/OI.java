e07f307bda80f08cedb2aeb5ced8883368f0c7c2
ckirschner <corey.kirschner@gmail.com>
2017-01-14 14:33:33 -0500

    Using dashboard for testing

--- src/org/usfirst/frc/team747/robot/OI.java
+++ src/org/usfirst/frc/team747/robot/OI.java
@@ -4,28 +4,38 @@ import org.usfirst.frc.team747.robot.commands.ShooterShootCommand;
 import org.usfirst.frc.team747.robot.maps.DriverStation;
 
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Preferences;
 import edu.wpi.first.wpilibj.buttons.Button;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class OI {
 
   private static final Joystick LEFT_JOYSTICK  = new Joystick(DriverStation.ControlMap.LEFT_JOYSTICK.getValue()),
                                 RIGHT_JOYSTICK = new Joystick(DriverStation.ControlMap.RIGHT_JOYSTICK.getValue());
   
+  static Preferences prefs;
+  
+  
   public OI(){
     Button fireButton  = new JoystickButton(LEFT_JOYSTICK, DriverStation.Joystick.TRIGGER.getValue());
     Button fireButton2 = new JoystickButton(RIGHT_JOYSTICK, DriverStation.Joystick.TRIGGER.getValue());
+    
     
     fireButton.whileHeld(new ShooterShootCommand());
     fireButton2.whileHeld(new ShooterShootCommand());
   }
 
   public static double getLeftShooterSpeed() {
-    return (LEFT_JOYSTICK.getRawAxis(DriverStation.Joystick.THROTTLE_AXIS.getValue()) - 1.0) / 2;
+		prefs = Preferences.getInstance();
+	  System.out.println(prefs.getDouble("Motor1", 0));
+	 return Math.abs(prefs.getDouble("Motor1", 0));
   }
 
   public static double getRightShooterSpeed() {
-    return (RIGHT_JOYSTICK.getRawAxis(DriverStation.Joystick.THROTTLE_AXIS.getValue()) - 1.0) / 2;
+		prefs = Preferences.getInstance();
+	  System.out.println(prefs.getDouble("Motor2", 0));
+	 return Math.abs(prefs.getDouble("Motor2", 0));
   }
 
 }
