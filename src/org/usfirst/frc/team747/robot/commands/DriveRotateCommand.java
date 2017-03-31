8d999b8f128ac13a3c2fbe31184843891a29cc53
PkmBrian <brianjimenez869@gmail.com>
2017-03-30 20:24:06 -0400

    Revert "Added proportonal speed threshold and slowing zone for auto."
    This reverts commit 4f825c736c094c5663c39d4ecc45cd28cf284058.

--- src/org/usfirst/frc/team747/robot/commands/DriveRotateCommand.java
+++ src/org/usfirst/frc/team747/robot/commands/DriveRotateCommand.java
@@ -11,11 +11,6 @@ public class DriveRotateCommand extends Command {
     
     private double speed;
     private double degrees;
-    
-    private final double threshold = 15; // Degrees
-    private final double minSpeed = 0.1;
-    
-    private final double precission = 1.0;
 
     public DriveRotateCommand(double speed, double degrees) {
         requires(Robot.DRIVE_TRAIN);
@@ -29,13 +24,6 @@ public class DriveRotateCommand extends Command {
     }
 
     protected void execute() {
-    	double remaining = remainingDegrees();
-    	double remainingAbs = Math.abs(remaining);
-        
-        if (remainingAbs < threshold) {
-        	speed = Math.max(remainingAbs/threshold, minSpeed) * speed / Math.abs(speed);
-        }
-        
         Robot.DRIVE_TRAIN.set(speed, -speed);
         
     }
@@ -45,19 +33,12 @@ public class DriveRotateCommand extends Command {
 //    	System.out.println("Robot: " + this.degrees);
     	boolean stopRotate = false;
     	
-    	double remaining = remainingDegrees();
-    	double remainingAbs = Math.abs(remaining);
-    	
-    	if (remainingAbs < precission){
+    	if (Robot.getNavXAngle() <= (this.degrees + 1) && Robot.getNavXAngle() >= (this.degrees - 1)){
 //    		System.out.println("****************SHOULD BE STOPPING!!!");
     		stopRotate = true;
     	}
     	return stopRotate;
         
-    }
-    
-    protected double remainingDegrees() {
-    	return this.degrees - Robot.getNavXAngle();
     }
 
     protected void end() {
